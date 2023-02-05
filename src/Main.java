fcaaa8a6fab8d361dc35be331cf5af3271dee448
Helge Fuchs <HelgeFuchs@gmx.net>
2023-02-05 21:22:00 +0100

    Final

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,15 @@ public class Main {
 
         for (int i = 1; i <= numbOfStars; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.println("x");
+                System.out.print("x");
             }
             System.out.println();
         }
         for (int i = numbOfStars - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                System.out.println("x");
+                System.out.print("x");
             }
             System.out.println();
         }
     }
+}
