6ab1c7d9c791842a959f7208fc84b90eed138443
LeRenard95 <HelgeFuchs@gmx.net>
2023-02-05 20:33:44 +0100

    ...

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("How many Stars?");
+        Scanner scanner = new Scanner(System.in);
+        int numbOfStars = scanner.nextInt();
+
+        for (int i = 1; i <= numbOfStars; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.println("x");
+            }
+            System.out.println();
+        }
+        for (int i = numbOfStars - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.println("x");
+            }
+            System.out.println();
+        }
+    }
